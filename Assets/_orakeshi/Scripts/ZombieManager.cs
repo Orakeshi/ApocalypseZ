c83ee72ece8ee8c4247e4233b0b15b55be156ec8
Tommy Webb <tommygeorgewebb@googlemail.com>
2021-08-03 15:49:27 +0100

    ✨ Zombie spawn script

---
+++ Assets/_orakeshi/Scripts/ZombieManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieManager : MonoBehaviour
+{
+    public Transform[] ZombieSpawnPoints;
+    public GameObject ZombiePrefab;
+
+    void Start()
+    {
+        SpawnNewZombie();
+    }
+
+    void SpawnNewZombie()
+    {
+        Instantiate(ZombiePrefab, ZombieSpawnPoints[0].transform.position, Quaternion.identity);
+    }
+}
