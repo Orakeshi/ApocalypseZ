0e003b491ecfe498e1371086576e722af22a7c98
Tommy Webb <tommygeorgewebb@googlemail.com>
2021-08-04 15:39:52 +0100

    Zombie changes

--- Assets/PathCreator/Examples/Scripts/PathFollower.cs
+++ Assets/PathCreator/Examples/Scripts/PathFollower.cs
@@ -24,6 +24,7 @@ namespace PathCreation.Examples
             if (pathCreator != null)
             {
                 distanceTravelled += speed * Time.deltaTime;
+
                 transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled, endOfPathInstruction);
                 transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled, endOfPathInstruction);
             }
