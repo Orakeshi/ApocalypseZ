d7dc51d7df801c1bfda104e6af59ab3dc98777da
Tommy Webb <tommygeorgewebb@googlemail.com>
2021-08-05 16:53:03 +0100

    ✨ Weapon spawn on menu selection

---
+++ Assets/_orakeshi/Scripts/DisableRay.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class DisableRay : MonoBehaviour
+{
+    public XRRayInteractor leftInteractorRay;
+    public XRRayInteractor rightInteractorRay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
