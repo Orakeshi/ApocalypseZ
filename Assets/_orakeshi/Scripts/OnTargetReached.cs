0b712cc62a76fa80bb557092c8d7322994583f5a
Tommy Webb <tommygeorgewebb@googlemail.com>
2021-08-07 13:12:35 +0100

    ✨ Magazine script to track ammo count

---
+++ Assets/_orakeshi/Scripts/OnTargetReached.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class OnTargetReached : MonoBehaviour
+{
+    public float threshold = 0.02f;
+    public Transform target;
+    public UnityEvent onReached;
+    private bool wasReached = false;
+    private void FixedUpdate()
+    {
+        float distance = Vector3.Distance(transform.position, target.position);
+
+        if(distance < threshold && !wasReached)
+        {
+            onReached.Invoke();
+            wasReached = true;
+        }
+        else if(distance >= threshold)
+        {
+            wasReached = false;
+        }
+    }
+}
