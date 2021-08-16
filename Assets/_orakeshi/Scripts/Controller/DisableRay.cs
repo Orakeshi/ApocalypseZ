f4f41e895bfea40782537b3645419c3063b54715
Tommy Webb <tommygeorgewebb@googlemail.com>
2021-08-16 12:00:31 +0100

    🎨 Added Orakeshi namespace to controller script

---
+++ Assets/_orakeshi/Scripts/Controller/DisableRay.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+namespace Orakeshi.ApocalypseZ.Controller
+{
+    public class DisableRay : MonoBehaviour
+    {
+        public XRRayInteractor leftInteractorRay;
+        public XRRayInteractor rightInteractorRay;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
+
