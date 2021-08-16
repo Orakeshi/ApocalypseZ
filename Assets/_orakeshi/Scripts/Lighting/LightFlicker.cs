140d0cc8b165bfdb24fb996661e375bc062a8661
Tommy Webb <tommygeorgewebb@googlemail.com>
2021-08-16 12:01:19 +0100

    🎨 Light namespace

---
+++ Assets/_orakeshi/Scripts/Lighting/LightFlicker.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Orakeshi.ApocalypseZ.Lighting
+{
+    public class LightFlicker : MonoBehaviour
+    {
+        private float timeOn = 0.8f;
+        private float timeOff = 0.6f;
+        private float changeTime = 0f;
+
+        private Light light;
+
+        private void Start()
+        {
+            light = this.gameObject.GetComponent<Light>();
+        }
+        void Update()
+        {
+            if (Time.time > changeTime)
+            {
+                light.enabled = !light.enabled;
+                if (light.enabled)
+                {
+                    changeTime = Time.time + timeOn;
+                }
+                else
+                {
+                    changeTime = Time.time + timeOff;
+                }
+            }
+        }
+    }
+}
+
