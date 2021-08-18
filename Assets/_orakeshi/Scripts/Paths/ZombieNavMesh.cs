3c444cf186a1f0fd6adc40fce837ee2bc185f444
Tommy Webb <tommygeorgewebb@googlemail.com>
2021-08-18 11:52:12 +0100

    ✨ Navmesh AI for zombies

---
+++ Assets/_orakeshi/Scripts/Paths/ZombieNavMesh.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Orakeshi.ApocalypseZ.Zombie
+{
+    public class ZombieNavMesh : MonoBehaviour
+    {
+        [SerializeField] public Transform movePositionTransform;
+
+        private NavMeshAgent navMeshAgent;
+        private void Awake()
+        {
+            navMeshAgent = GetComponent<NavMeshAgent>();
+        }
+
+        private void Update()
+        {
+            navMeshAgent.destination = movePositionTransform.position;
+        }
+    }
+}
+
