d8884a489711f837d830cfc7d6e7aaf58b3cd6a9
Tommy Webb <tommygeorgewebb@googlemail.com>
2021-08-18 11:52:57 +0100

    🎨 New routes for zombies & Navmesh fix

--- Assets/_orakeshi/Scripts/Zombies/ZombieManager.cs
+++ Assets/_orakeshi/Scripts/Zombies/ZombieManager.cs
@@ -13,6 +13,8 @@ namespace Orakeshi.ApocalypseZ.Zombie
 
         public PathCreator[] routes;
 
+        public Transform Player;
+
         void Start()
         {
             StartCoroutine(ZombieSpawn());
@@ -22,29 +24,31 @@ namespace Orakeshi.ApocalypseZ.Zombie
         void SpawnNewZombie()
         {
             int random;
-            random = Random.Range(0, 1);
+            random = Random.Range(0, 10);
             //Instantiate(ZombiePrefab, ZombieSpawnPoints[random].transform.position, Quaternion.identity);
         }
 
         IEnumerator ZombieSpawn()
         {
             int random;
-            random = Random.Range(0, 2);
+            random = Random.Range(0, 9);
 
             int randomZombie = Random.Range(0, ZombiePrefab.Length);
 
             GameObject currentZombie = Instantiate(ZombiePrefab[randomZombie], ZombieSpawnPoints[random].transform.position, Quaternion.identity);
 
-            if (random == 0)
-            {
-                currentZombie.GetComponent<PathFollower>().pathCreator = routes[0];
-                //ZombiePrefab.GetComponent<PathFollower>().pathCreator = routes[0];
-            }
-            else
-            {
-                currentZombie.GetComponent<PathFollower>().pathCreator = routes[1];
-                //ZombiePrefab.GetComponent<PathFollower>().pathCreator = routes[1];
-            }
+            //if (random == 0)
+            //{
+            //    currentZombie.GetComponent<PlayerNavMesh>().movePositionTransform = Player;
+            //    //ZombiePrefab.GetComponent<PathFollower>().pathCreator = routes[0];
+            //}
+            //else
+            //{
+            //    currentZombie.GetComponent<PlayerNavMesh>().movePositionTransform = Player;
+            //    //ZombiePrefab.GetComponent<PathFollower>().pathCreator = routes[1];
+            //}
+
+            currentZombie.GetComponent<ZombieNavMesh>().movePositionTransform = Player;
 
             yield return new WaitForSeconds(10f);
             StartCoroutine(ZombieSpawn());
