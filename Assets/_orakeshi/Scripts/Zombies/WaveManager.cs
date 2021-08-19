f16e1dae539b781dd1320dc20725335d778dce8a
Tommy Webb <tommygeorgewebb@googlemail.com>
2021-08-19 17:17:38 +0100

    ✨ Wave manager script

---
+++ Assets/_orakeshi/Scripts/Zombies/WaveManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Orakeshi.ApocalypseZ.Zombie
+{
+    public class WaveManager : MonoBehaviour
+    {
+        int totalDeadZombies = 0;
+        float timeToSpawn = 10;
+        [SerializeField] public ZombieManager zombieManager;
+
+        private void Start()
+        {
+            StartCoroutine(ZombieSpawnWrapper());
+        }
+
+        IEnumerator ZombieSpawnWrapper()
+        {
+            yield return new WaitForSeconds(timeToSpawn);
+            NextWave();
+        }
+
+        void NextWave()
+        {
+            if(totalDeadZombies % 20 == 0)
+            {
+                zombieManager.ZombieSpawn("Boss");
+            }
+            else
+            {
+                zombieManager.ZombieSpawn("Zombie");
+                StartCoroutine(ZombieSpawnWrapper());
+            }
+            
+        }
+    }
+}
+
