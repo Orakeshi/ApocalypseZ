88b9c4a9acf8eadeb5a3aed3877ef1e1139a35fd
Tommy Webb <tommygeorgewebb@googlemail.com>
2021-08-19 12:47:57 +0100

    ✨ BulletFX Script

---
+++ Assets/_orakeshi/Scripts/Weapons/BulletManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Orakeshi.ApocalypseZ.Weapon
+{
+    public class BulletManager : MonoBehaviour
+    {
+        public GameObject[] bulletFX;
+        private void OnCollisionEnter(Collision other)
+        {
+            
+            if (other.gameObject.tag == "Tree")
+            {
+                foreach(var FX in bulletFX)
+                {
+                    if (FX.name.Contains("Wood"))
+                    {
+                        Instantiate(FX);
+                        FX.transform.position = gameObject.transform.position;
+                        Destroy(this.gameObject);
+                        Destroy(FX, 1);
+                    }
+                }
+
+            }
+            //else if (other.gameObject.tag == "Zombie")
+            //{
+
+            //}
+            else
+            {
+                Instantiate(bulletFX[0]);
+                bulletFX[0].transform.position = gameObject.transform.position;
+                Destroy(this.gameObject);
+                Destroy(bulletFX[0], 1);
+                
+            }
+        }
+    }
+}
+
