f917b93da795c914154144d80622fed9c21265e0
Tommy Webb <tommygeorgewebb@googlemail.com>
2021-08-21 10:55:09 +0100

    Weapon class to maintain various prefabs

---
+++ Assets/_orakeshi/Scripts/Weapons/Weapon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Orakeshi.ApocalypseZ.Weapon
+{
+    [Serializable]
+    public class Weapon
+    {
+        public GameObject prefab;
+        public GameObject bullet;
+        public GameObject bulletCasing;
+    }
+}
+
