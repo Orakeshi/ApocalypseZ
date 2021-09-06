be49ae52a8497bcb5df6c04b06a5f3a2ab7b5ee3
Tommy Webb <tommygeorgewebb@googlemail.com>
2021-09-06 22:59:09 +0100

    Implemented Event system for game

---
+++ Assets/GameEvents.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEvents : MonoBehaviour
+{
+    public static GameEvents current;
+
+    private void Awake()
+    {
+        current = this;
+    }
+
+    public event Action onZombieTriggerSpawn;
+
+    public void ZombieTriggerSpawn()
+    {
+        if (onZombieTriggerSpawn != null)
+        {
+            onZombieTriggerSpawn();
+        }
+    }
+
+    public event Action onZombieTriggerDeath;
+    public void ZombieTriggerDeath()
+    {
+        if (onZombieTriggerDeath != null)
+        {
+            onZombieTriggerDeath();
+        }
+    }
+}
